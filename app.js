2a7b8faf55d119b689eaa86f8a40083a0ab43566
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-15 22:01:35 -0500

    node: promises and modules

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-
-const fs = require('fs');
-
+const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
-console.log('=======================');
-console.log('  Tabla del número 5');
-console.log('=======================');
 
-// Imprimir la tabla del 5
+const base = 151;
 
-const base = 13;
-let salida = '';
-
-for( let i = 1; i <= 10; i++ ) {
-    salida += `${ base } x ${ i } = ${ base * i }\n`;
-}
-
-console.log( salida );
-
-// fs.writeFile(`tabla-${ base }.txt`, salida, ( err ) => {
-//     if (err) throw err;
-
-//     console.log(`tabla-${ base } has been created`);
-// });
-
-fs.writeFileSync( `tabla-${ base }.txt`, salida );
-
-console.log(`tabla-${ base }.txt has been created`);
+crearArchivo( base )
+    .then( nombreArchivo => console.log(nombreArchivo, 'created') )
+    .catch( err => console.log(err) );
