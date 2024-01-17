94d05bde3b9cd229d9f1879f9a5f4a4cf43a2637
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-16 21:28:58 -0500

    inputs from CLI

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
 
-const base = 151;
+
+// console.log( process.argv );
+// const [ , , arg3] = process.argv;
+const [ , , arg3 = 'base=5'] = process.argv;
+const [ , base = 5 ] = arg3.split('=');
+
+// console.log( base );
+// console.log( arg3 );
+// const base = 5;
 
 crearArchivo( base )
     .then( nombreArchivo => console.log(nombreArchivo, 'created') )
