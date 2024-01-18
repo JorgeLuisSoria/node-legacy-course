cac207bf36e8219b5b9216eaf42f5b43d228033a
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-18 12:18:39 -0500

    repository updated

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -18,7 +18,7 @@ const crearArchivo = async( base = 5, listar = true, hasta = 10 ) => {
         
         if( listar ) { console.log( salida ) };        
     
-        fs.writeFileSync( `tabla-${ base }.txt`, salida );
+        fs.writeFileSync( `./salida/tabla-${ base }.txt`, salida );
     
         return `tabla-${ base }.txt`;
         
